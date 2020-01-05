09e63e6763e436df032ec6d99ece89096e30ccac
slonegd <slonkdv@mail.ru>
2020-01-05 16:14:17 +0300

    little simplefy

--- registers_set.hpp
+++ registers_set.hpp
@@ -14,24 +14,19 @@ static_assert(shift(24) == 3);
 template<class Register, class Arg>
 constexpr std::size_t arg_value(type_identity<Register>, Arg arg) {
     constexpr auto arg_traits = traits(type_identity<Arg>{});
-    // нужен тип со свойствами, чтобы достать маску
-    using Arg_traits = TYPE(arg_traits);
-    constexpr auto arg_shift = shift(Arg_traits::mask);
     // значение вычисляется только, если аргумент соотвествует регистру
-    return std::is_base_of_v<Arg_traits, Register> ? (static_cast<std::size_t>(arg) << arg_shift) : 0;
+    if constexpr (not std::is_base_of_v<TYPE(arg_traits), Register>)
+        return 0;
+
+    constexpr auto mask = decltype(arg_traits)::type::mask;
+    constexpr auto arg_shift = shift(mask);
+    return static_cast<std::size_t>(arg) << arg_shift;
 }
 
 // определяем значение конкретного регистра по всем аргументам
 template<class Register, class...Args>
 constexpr std::size_t register_value(type_identity<Register> reg, Args...args) {
-    std::size_t value {0};
-    // бинарные опреаторы недопустимы в выражениях развёртки, поэтому делаем лямбду-обёртку
-    auto change = [](auto& value, auto arg_value) {
-        value |= arg_value;
-    };
-    // цикл по всем аргументам, с записью результата в переменную
-    (change(value, arg_value(reg, args)), ...);
-    return value;
+    return (arg_value(reg, args) | ...);
 }
 
 // непосредственно запись в регистр, нужны только адрес и значение
@@ -54,16 +49,17 @@ constexpr void set(type_pack<Registers...> registers, std::size_t address, Args.
     }), "one of arguments in set method don`t belong to periph type");
 
     // определяем список регистров, в которые надо записывать данные
-    constexpr auto registers_for_write = filter(registers, [=](auto reg){
+    constexpr auto registers_for_write = filter(registers, [](auto reg){
         return any_of(args_traits, [](auto arg){
+            // интересно, почему reg оказался в этой лямбде без захвата?
             return std::is_base_of_v<TYPE(arg), TYPE(reg)>;
         });
     });
 
     // определяем значения в каждом регистре и пишем в его
-    foreach(registers_for_write, [&](auto i, auto reg){
+    foreach(registers_for_write, [=](auto i, auto reg){
         auto value = register_value(reg, args...);
-        auto offset = decltype(reg)::type::offset;
+        constexpr auto offset = decltype(reg)::type::offset;
         write(address + offset, value);
     });
 };
